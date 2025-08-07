08fdf91cfdda7825b2b07e66f536a6346c96f000
Vinicius <48769407+ViniciusCoelhoNascimento@users.noreply.github.com>
2025-08-07 20:06:21 -0300

    Comentarios

--- src/app/comments-session/comments-session.component.ts
+++ src/app/comments-session/comments-session.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { DbService } from '../db.service';
+import { inject } from '@angular/core';
+import { comentario } from '../comentario';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-comments-session',
@@ -22,9 +26,13 @@ import { FormsModule } from '@angular/forms';
           </div>
         </div>
 
-        <textarea readonly *ngFor="let comentario of comments" >
-          {{comentario}}
-        </textarea>
+        <div class="comment" *ngFor="let comentario of comments">
+          <div class="comment-header">
+            <span class="username">{{comentario.username}}</span>
+            <span class="timestamp">{{comentario.data_hora | date:'dd/MM/yyyy HH:mm'}}</span>
+          </div>
+          <div class="comment-text">{{comentario.comentario}}</div>
+        </div>
 
         </div>
       </div>
@@ -32,12 +40,42 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './comments-session.component.css'
 })
 export class CommentsSessionComponent {
+  @Input() itemType: string = '';
+  @Input() itemId: number = 0;
   comment: string = '';
-  comments: string[] = ['comentário 1', 'comentário 2'];
+  comments: comentario[] = [];
+  dbService: DbService = inject(DbService);
+  
+
+  ngOnInit() {
+    // Agora as propriedades @Input() já estão definidas
+    this.dbService.getComments(this.itemType, this.itemId).then((lcomments: comentario[]) => {
+      this.comments = lcomments;
+    });
+    console.log('comentario: itemtype:' + this.itemType + '  ' + ' itemId:' + this.itemId);
+  }
 
   postComment() {
-    if (this.comment.trim()){
-      this.comment = '';
+    if (this.comment.trim()) {
+      const newComment: comentario = {
+        id: null,
+        id_usuario: localStorage.getItem('userId') || "0",
+        itemType: this.itemType,
+        itemId: this.itemId,
+        comentario: this.comment,
+        data_hora: new Date(),
+        username: localStorage.getItem('username')
+      };
+
+      console.log('Novo comentário:', newComment);
+  
+      this.dbService.postComment(newComment).then((savedComment) => {
+        console.log('Comentário salvo:', savedComment);
+        this.comments.push(savedComment); // Adiciona o comentário à lista
+        this.comment = ''; // Limpa o campo de comentário
+      }).catch((error) => {
+        console.error('Erro ao salvar comentário:', error);
+      });
     }
   }
 }
--- src/app/db.service.ts
+++ src/app/db.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Book } from './book';
 import { Movie } from './movie';
 import { Video } from './video';
+import { User } from './user';
+import { comentario } from './comentario';
 
 @Injectable({
   providedIn: 'root'
@@ -21,6 +23,36 @@ export class DbService {
     }    
 
     return await response.json();
+  }
+
+  async getUser() : Promise<User> {
+    const response = await fetch(this.url + 'auth/register');
+    const json = await response.json();
+    
+    localStorage.setItem('userId',json.userId);
+    localStorage.setItem('jndb-token',json.token);
+    localStorage.setItem('username', json.username);
+    
+    return json as User;
+  }
+
+  async postComment(comentario : comentario) : Promise<comentario> {
+    const token = localStorage.getItem('jndb-token');
+    const response = await fetch(this.url + 'user-activity/post', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify(comentario)
+    });
+    const json = await response.json();
+    return json as comentario;
+  }
+
+  async getComments(itemType: string, itemId: number) : Promise<comentario[]> {
+    const data = await fetch(this.url + 'user-activity/comentarios?itemType=' + itemType + '&itemId=' + itemId);
+    return await data.json() ?? [];
   }
 
   async getAllBooks() : Promise<Book[]> {
@@ -48,7 +80,7 @@ export class DbService {
   async getRelatedVideos(bookId: number) : Promise<Video[]> {
     const data = await fetch(this.url + 'api/books/' + bookId + '/videos');
     const json = await data.json() ?? [];
-    console.log(json)
+    //console.log(json)
     //return json as Video[];
     return json;
   }
@@ -56,7 +88,7 @@ export class DbService {
   async getMoviesRelatedVideos(movieId: number) : Promise<Video[]> {
     const data = await fetch(this.url + 'api/movies/' + movieId + '/videos');
     const json = await data.json() ?? [];
-    console.log(json)
+    //console.log(json)
     //return json as Video[];
     return json;
   }
